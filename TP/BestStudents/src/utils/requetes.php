7bbcc4c3ca26a6a99bffc8bdc09c1b8c96d7fd46
EstebanRacine <esteban.racineecole@gmail.com>
2023-02-05 13:10:14 +0100

    function getStudentById

--- TP/BestStudents/src/utils/requetes.php
+++ TP/BestStudents/src/utils/requetes.php
@@ -8,3 +8,13 @@ function getAllStudents():array|bool{
     $requete->execute();
     return $requete->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function getStudentById($id){
+    $connexion = createConnection();
+    $requeteSQL = "SELECT * FROM etudiants WHERE id = :id";
+    $requete = $connexion->prepare($requeteSQL);
+    $requete->bindValue("id", $id);
+    $requete->execute();
+    return $requete->fetch(PDO::FETCH_ASSOC);
+
+}
