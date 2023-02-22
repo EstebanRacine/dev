728b67f875d37e219cb6ee7b0f748fba55b84269
Esteban Racine <esteban.racineecole@gmail.com>
2023-02-22 17:54:19 +0100

    Header, Nav, Footer fichier exterieur

---
+++ TP/BestStudents/src/headerEtNav.php
@@ -0,0 +1,12 @@
+<header>
+    <img src="../src/images/BS_Logo.png" alt="Logo de Best Students">
+    <h1>Best Students</h1>
+</header>
+
+<nav>
+    <a href="../index.php">Accueil</a>
+    <a href="../create-student.php">Ajouter un étudiant</a>
+    <a href="../cherchePromo.php">Liste des promotions</a>
+    <a href="../contact.php">Contact</a>
+</nav>
+
