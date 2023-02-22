0651276f34afc4314c78a989c875ec6b84356e21
Esteban Racine <esteban.racineecole@gmail.com>
2023-02-22 16:12:44 +0100

    Etudiants par Promo

--- TP/BestStudents/index.php
+++ TP/BestStudents/index.php
@@ -3,7 +3,6 @@ include "src/utils/requetes.php";
 include "src/utils/date.php";
 include "src/utils/fonctions.php";
 $arrayStudents = getAllStudents();
-$student = getStudentByID(1);
 ?>
 <!doctype html>
 <html lang="fr">
