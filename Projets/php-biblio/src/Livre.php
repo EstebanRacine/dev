245041603d2caf6b8a2546eb4be135aabb6c944c
Esteban Racine <esteban.racineecole@gmail.com>
2023-05-09 09:49:04 +0200

    Editeur + Categorie dans LIVRE

--- Projets/php-biblio/src/Livre.php
+++ Projets/php-biblio/src/Livre.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once "Auteur.php";
+require_once "Editeur.php";
+require_once "Categorie.php";
 
 class Livre{
     private string $isbn;
@@ -8,6 +10,8 @@ class Livre{
     private int $nbPages;
     private DateTime $dateParution;
     private Auteur $auteur;
+    private Categorie $categorie;
+    private Editeur $editeur;
 
 
     /**
@@ -17,12 +21,14 @@ class Livre{
      * @param DateTime $dateParution
      * @param Auteur $auteur
      */
-    public function __construct(string $isbn, string $titre, int $nbPages, DateTime $dateParution, Auteur $auteur){
+    public function __construct(string $isbn, string $titre, int $nbPages, DateTime $dateParution, Auteur $auteur, Categorie $categorie, Editeur $editeur){
         $this->titre = $titre;
         $this->isbn = $isbn;
         $this->dateParution = $dateParution;
         $this->nbPages = $nbPages;
         $this->auteur = $auteur;
+        $this->editeur = $editeur;
+        $this->categorie = $categorie;
     }
 
 
@@ -62,7 +68,9 @@ class Livre{
         $nbPage = "Nb pages : ".$this->nbPages.PHP_EOL;
         $dateParu = "Date de parution : ". $this->dateParution->format("d/m/Y").PHP_EOL;
         $auteur = "Auteur : ".$this->auteur->getId().PHP_EOL;
-        return $isbn.$titre.$auteur.$dateParu.$nbPage;
+        $editeur = "Editeur : ".$this->editeur->getInfos().PHP_EOL;
+        $categorie = "Catégorie : ".$this->categorie->getLibelle().PHP_EOL;
+        return $isbn.$titre.$auteur.$dateParu.$categorie.$nbPage.$editeur;
     }
 
 }
--- Projets/php-biblio/test/index.php
+++ Projets/php-biblio/test/index.php
@@ -2,11 +2,15 @@
 
 require_once "../src/Auteur.php";
 require_once "../src/Livre.php";
+require_once "../src/Editeur.php";
+require_once "../src/Categorie.php";
 
 $orwell = new Auteur("Orwell", "Georges");
+$hachette = new Editeur("Hachette", "Bordeaux");
+$roman = new Categorie("Roman");
 
 $fermeAnimaux = new Livre("2851841203", "La Ferme des Animaux", 92, date_create_from_format("d/m/Y",
-    "17/08/1945"), $orwell);
+    "17/08/1945"), $orwell, $roman, $hachette);
 
 $auteur = $fermeAnimaux->getAuteur();
 echo $auteur->getId().PHP_EOL;
